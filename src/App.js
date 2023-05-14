cc5cc64d82f39d493e94f453f753fb41f0041612
bloodstrawberry <vvv3334@hanmail.net>
2023-05-15 00:30:44 +0900

    first submit
    first submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+//App.js 
+import { Route, Link, Routes } from "react-router-dom";
+
+//routing components
+import Home from "./page/Home";
+import Router1 from "./page/Router1";
+import Router2 from "./page/Router2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Link to="/">홈</Link>
+        <Link to="/r1">Router1</Link>
+        <Link to="/r2">Router2</Link>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/r1" element={<Router1 />} />
+          <Route path="/r2" element={<Router2 />} />
+        </Routes>
+      </div>
     </div>
   );
 }
